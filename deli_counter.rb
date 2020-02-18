963c33363e163f26660f80c99cd8a3074610fd15
Elijah wines <elijahwines84@yahoo.com>
2020-02-18 14:55:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 katz_deli = []
 
 def line(katz_deli)
-  katsz_deli.each do |name_in_line|
+  katz_deli.each do |name_in_line|
     puts "current line: #{name_in_line} is number #{katz_deli.length} in line."
   end
   if katz_deli.length === 0
