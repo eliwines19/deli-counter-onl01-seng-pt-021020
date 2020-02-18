287c06a38c04a3cc905022749d4d913beb9671ca
Elijah wines <elijahwines84@yahoo.com>
2020-02-18 15:02:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(katz_deli)
   katz_deli.each do |name_in_line|
-    puts "The line is currently: #{katz_deli.length}. #{name_in_line}"
+    puts "The line is currently: #{katz_deli[0]}. #{katz_deli.first}"
   end
   if katz_deli.length === 0
     puts "The line is currently empty."
