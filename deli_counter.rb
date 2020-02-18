6f1aa92ddf78f7a5a1d81f3832339369952dbf7f
Elijah wines <elijahwines84@yahoo.com>
2020-02-18 15:13:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,25 @@
 katz_deli = []
 
 def line(katz_deli)
-  katz_deli.each do |name_in_line|
-    puts "The line is currently: 1. #{katz_deli.first} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
-  end
-  if katz_deli.length === 0
+  if katz_deli.empty?
     puts "The line is currently empty."
+  else
+    puts "The line is currently 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  spot = katz_deli.index(name) + 1
+  puts "Welcome, #{name} you are number #{spot} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    serving = katz_deli[0]
+    puts "currently serving #{serving}."
+    katz_deli.shift
   end
 end
