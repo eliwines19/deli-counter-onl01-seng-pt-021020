fcdf140aa5fa855125fd62422f25fb357a19340a
Elijah wines <elijahwines84@yahoo.com>
2020-02-18 14:54:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 katz_deli = []
+
+def line(katz_deli)
+  katsz_deli.each do |name_in_line|
+    puts "current line: #{name_in_line} is number #{katz_deli.length} in line."
+  end
+  if katz_deli.length === 0
+    puts "there is nobody in line"
+  end
+end
